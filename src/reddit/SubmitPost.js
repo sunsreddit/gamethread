deeed3a15a5c9a7b0b8fe43b8bfa6a5127274398
Phoenix Suns Subreddit <bruxc@sunsreddit.net>
2022-12-03 15:16:42 -0500

    [v0.1.0-beta] Refines submission formatting functions

--- src/reddit/SubmitPost.js
+++ src/reddit/SubmitPost.js
@@ -1,6 +1,6 @@
 import snoowrap from "snoowrap"
 
-export async function Post(sub, title, text, flairId) {
+export function SubmitPost(sub, title, text, flairId) {
     return new snoowrap({
         userAgent: process.env.USER_AGENT,
         clientId: process.env.CLIENT_ID,
@@ -11,44 +11,16 @@ export async function Post(sub, title, text, flairId) {
         title
     })
         .selectFlair({
-            flair_template_id: flair ? flair : ""
+            flair_template_id: flairId ? flairId : ""
         })
         .sticky()
         .distinguish()
         .setSuggestedSort('new')
 }
 
-export const Title = (matchup, time) => {
-    return `[GAME THREAD]: ${matchup} (${time})`
-}
+export const Title = (matchup, time) => { return `[GAME THREAD]: ${matchup} (${time})` }
 
-const hScore = {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    total() {
-        return this[1] + this[2] + this[3] + this[4]
-    }
-}
-const aScore = {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    total() {
-        return this[1] + this[2] + this[3] + this[4]
-    }
-}
-
-const _boxScore = (home, away) => {
-    return `
-|            |1st         |2nd         |3rd         |4th         |Final            |
-|:--         |:--         |:--         |:--         |:--         |:--              |
-|${home.name}|${hScore[1]}|${hScore[2]}|${hScore[3]}|${hScore[4]}|${hScore.total()}|
-|${away.name}|${aScore[1]}|${aScore[2]}|${aScore[3]}|${aScore[4]}|${aScore.total()}|
-`
-}
+const header = `|   |   |\n|:--|:--|`
 
 function _arena(home) {
     return `|**Arena**|${home.arena_city}, ${home.arena_state} - ${home.arena_name}|`
@@ -63,11 +35,7 @@ function _media(home, away) {
 }
 
 export function Body(home, away) {
-    // const _score = _boxScore(home, away)
     const ad = _arena(home)
     const md = _media(home.media, away.media)
-    return `|   |   |
-|:--|:--|
-${ad}
-${md}`
+    return `${header}\n${ad}\n${md}`
 }
