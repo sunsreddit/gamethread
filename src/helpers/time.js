fcb8bdde76bf5d31070d3759937e01e438e2f00e
Phoenix Suns Subreddit <bruxc@sunsreddit.net>
2022-12-03 15:02:43 -0500

    [v0.1.0-beta] Removes old time helper functions

--- src/helpers/time.js
+++ src/helpers/time.js
@@ -3,7 +3,7 @@
  * @param {*} date - A Date Object
  * @returns {Boolean}
  */
-function IsDaylightSavings(date = new Date()) {
+export function IsDaylightSavings(date = new Date()) {
     const year = date.getFullYear()
     const dst_start = new Date(year, 2, 14)
     const dst_end = new Date(year, 10, 7)
@@ -13,38 +13,26 @@ function IsDaylightSavings(date = new Date()) {
 }
 
 /**
- * Returns the difference in hours between two given dates
- * @param {Date} startDate - The current date
- * @param {Date} endDate - The game date
- * @returns {Boolean}
+ * Subtracts specified time period in minutes from given `date` instance.
+ * 
+ * @param {Date} date Date instance to subtract minutes
+ * @param {number} amount Number of minutes to subtract 
  */
-function diffHours(startDate, endDate) {
-    if (!(startDate instanceof Date || endDate instanceof Date)) throw new Error(`Parameters must be of type [Date]`)
-    return Math.abs(Math.round(((endDate.getTime() - startDate.getTime())) / (1000 * 3600)))
-}
-
-/**
- * Checks the time and confirms if it's within the two-hour window to Submit a Game Thread Post
- * @param {*} gameTime - A string timestamp representing the time of when the game starts  
- * @param {*} postHoursPrior - An integer representing how many hours before the game starts (Default: 2 hours)
- * @returns {Boolean}
- */
-export function IsPostTime(gameTime, postHoursPrior = 2) {
-    if (!(typeof gameTime === 'string')) throw new Error(`Parameters must be of type [String]`)
-    const diff = diffHours(new Date(), new Date(gameTime))
-    return diff === (IsDaylightSavings() ? postHoursPrior + 1 : postHoursPrior) // Review this offset, possibly wrong
-}
+ export function SubtractMinutes(date, amount) {
+    const newDate = new Date(date);
+    return new Date(newDate.setMinutes(newDate.getMinutes() - amount))
+  }
 
 /**
  * Returns a cron schedule using a Date object
  * @param {Date} date - A Date Object
- * @returns {String} - Cron schedule
+ * @returns {string} - Cron schedule
  */
-export async function Date2Cron(date = new Date()) {
+export function Date2Cron(date = new Date()) {
     const minute = date.getMinutes()
     const hour = date.getHours()
     const dayOfMonth = date.getDate()
-    const month = date.getMonth() + 1
+    const month = date.getMonth() + 1 
     const dayOfWeek = date.getDay()
     return `${minute} ${hour} ${dayOfMonth} ${month} ${dayOfWeek}`
 }
