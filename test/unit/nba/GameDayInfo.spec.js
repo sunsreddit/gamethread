30df46b52c3f77fdf67bbe1b21ea3d211cc16d0f
Phoenix Suns Subreddit <bruxc@sunsreddit.net>
2022-12-12 23:15:46 -0500

    [v0.1.0-beta] Adds type error unit test for GameDayInfo

--- test/unit/nba/GameDayInfo.spec.js
+++ test/unit/nba/GameDayInfo.spec.js
@@ -1,1 +1,9 @@
-{}
+import { GameDayInfo } from "../../../src/nba/GameDayInfo.js"
+import { default as data } from "../nba/game.data.json" assert { type: "json" }
+
+// GameDayInfo()
+describe("Game Day Data Testing", () => {
+    test("Throws a type error", async () => {
+        await expect(GameDayInfo()).rejects.toThrow(TypeError)
+    })
+})
