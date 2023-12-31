ff384dd557c72db1ed5d8b52c3c4113b74af5508
bruxc <bruxc@sunsreddit.net>
2023-12-31 15:26:19 -0500

    Improves error handling and overall readability for GameMedia module

--- src/nba/GameMedia.mjs
+++ src/nba/GameMedia.mjs
@@ -6,39 +6,42 @@
 export async function GameMediaInfo(gameInfo) {
   if (typeof gameInfo !== 'object')
     throw new TypeError(`GameDayInfo: Parameter is not of type 'Object'`);
+  
   async function _getMedia(scope, type) {
     return gameInfo.b.filter(
       (obj) => obj.scope === scope && obj.type === type
     )[0];
   }
-  const awayRadio = await _getMedia('away', 'radio');
-  const homeRadio = await _getMedia('home', 'radio');
-  const awayTV = await _getMedia('away', 'tv');
-  const homeTV = await _getMedia('home', 'tv');
+
   const _defaultStreams = {
     ddg: 'https://duckduckgo.com/?q=%21ducky+%5C',
     nba: 'https://www.nba.com/watch/league-pass-stream',
   };
+  
+  const radioInfo = (team) => {
+    const radio = _getMedia(team, 'radio');
+    return {
+      name: radio?.disp || 'N/A',
+      url: radio?.url || encodeURIComponent(_defaultStreams.ddg + radio?.disp || '')
+    };
+  };
+
+  const tvInfo = (team) => {
+    const tv = _getMedia(team, 'tv');
+    return {
+      name: tv?.disp || 'NBA League Pass',
+      url: tv?.url ||(tv?.disp ? encodeURIComponent(_defaultStreams.ddg + tv.disp) : _defaultStreams.nba),
+    };
+  };
+
   return {
     away: {
-      radio: {
-        name: awayRadio.disp,
-        url: awayRadio.url || `${_defaultStreams.ddg}${awayRadio.disp}`,
-      },
-      tv: {
-        name: awayTV?.disp || 'NBA Stream',
-        url: awayTV?.url || _defaultStreams.nba,
-      },
+      radio: radioInfo('away'),
+      tv: tvInfo('away'),
     },
     home: {
-      radio: {
-        name: homeRadio.disp,
-        url: homeRadio.url || `${_defaultStreams.ddg}${homeRadio.disp}`,
-      },
-      tv: {
-        name: homeTV?.disp || 'NBA Stream',
-        url: homeTV?.url || _defaultStreams.nba,
-      },
+      radio: radioInfo('home'),
+      tv: tvInfo('home'),
     },
     misc: {
       livestream: 'https://gprivate.com/620jy',
