79775d325b46491cd9b9c449917fc99849aff838
bruxc <bruxc@sunsreddit.net>
2024-01-01 09:27:15 -0500

    Removing old Unit Tests

--- __tests__/GameMedia.spec.mjs
+++
@@ -1,8 +0,0 @@
-import { GameMediaInfo } from '../../../src/nba/GameMedia.mjs';
-
-// GameDayInfo()
-describe('Game Day Data Testing', () => {
-  test('Throws a type error', async () => {
-    await expect(GameMediaInfo()).rejects.toThrow(TypeError);
-  });
-});
